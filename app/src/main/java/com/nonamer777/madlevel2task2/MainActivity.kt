7c9e590f46e296da9b3e9edede58d18c47c1e048
Oscar Wellner <o.wellner@telfort.nl>
2020-09-02 01:29:33 +0200

    added swiping logic to determine when to remove a statement from the list

--- app/src/main/java/com/nonamer777/madlevel2task2/MainActivity.kt
+++ app/src/main/java/com/nonamer777/madlevel2task2/MainActivity.kt
@@ -44,3 +44,58 @@ class MainActivity : AppCompatActivity() {
             statements.add(Statement(Statement.STATEMENTS[i], Statement.STATEMENT_CONDITION[i]))
         }
         statementAdapter.notifyDataSetChanged()
+
+        // Attaches the created ItemTouchHelpers to the view.
+        createItemTouchHelper(ItemTouchHelper.LEFT).attachToRecyclerView(rvStatements)
+        createItemTouchHelper(ItemTouchHelper.RIGHT).attachToRecyclerView(rvStatements)
+    }
+
+    /** Creates an ItemTouchHelper in for a certain swipe direction. */
+    private fun createItemTouchHelper(swipeDirection: Int): ItemTouchHelper {
+
+        val callback = object: ItemTouchHelper.SimpleCallback(0, swipeDirection) {
+
+            // Disable moving items up or down in the list.
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean = false
+
+            // Handles swiping an item
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+                val condition = statements[position].isTruth
+
+                /* Remove an item when the its condition is `false` and the direction of the swipe is left. (4)
+                 * Remove an item when the its condition is `true` and the direction of the swipe is right. (8) */
+                when (direction) {
+                    4 -> removeStatement(!condition, position)
+                    8 -> removeStatement(condition, position)
+                }
+            }
+        }
+        return ItemTouchHelper(callback)
+    }
+
+    /**
+     * Removes a statement from the list if the user had swiped the statement to the right direction.
+     * Otherwise show a Snackbar.
+     * */
+    private fun removeStatement(answerIsCorrect: Boolean, index: Int) {
+        if (answerIsCorrect) {
+            statements.removeAt(index)
+            statementAdapter.notifyDataSetChanged()
+            return
+        }
+        /* Resets the swiped item back to its original position after it had been swiped in the
+         * wrong direction and show the Snackbar. */
+        statementAdapter.notifyDataSetChanged()
+
+        Snackbar.make(
+            rvStatements,
+            "Sorry, your answer is incorrect and thus we can not remove the statement.",
+            Snackbar.LENGTH_LONG
+        ).show()
+    }
+}
